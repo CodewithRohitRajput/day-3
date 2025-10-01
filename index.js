2df56c112d16c985708fc846c55bc95e5f73c495
CodeWithRohitRajput <example@gmail.com>
2025-10-01 18:41:14 +0530

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+let index = 0;
+let colors = ["red", "blue", "purple", "green", "yellow"];
+
+let myButton = document.getElementById("btn");
+let myContainer = document.getElementsByClassName("container")[0];
+
+myButton.addEventListener("click", () => {
+    myContainer.style.background = colors[index++];
+    if (index > colors.length - 1) {
+        index = 0;
+    } 
+})
